6baeb16241fe385a68e2967bc44c55b6ace3dd35
zmk8 <maikhanh88888888@gmail.com>
2022-07-15 20:31:03 +0700

    Next 1

--- test1.cpp
+++ test1.cpp
@@ -3,5 +3,10 @@
 using namespace std;
 
 int main(){
-	cout << "Hello word";
+	cout << "QUAN LY SINH VIEN";
+	cout << "\n1. Them sinh vien.";
+	cout << "\n2. Cap nhat sinh vien.";
+	cout << "\n3. Xoa sinh vien.";
+	cout << "\n4.";
+       	
 }
